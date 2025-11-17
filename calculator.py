a4a1f25e6d91bacc117a7c1f488530501a86ddfb
Ira Narisma <narisma.ira.chloie@gmail.com>
2025-11-17 21:56:36 +0800

    more style

--- calculator.py
+++ calculator.py
@@ -1,6 +1,6 @@
 import tkinter 
 
-buttons = [
+button_values = [
      ["AC", "+/-", "%", "÷"],
      ["7", "8", "9", "×"],
      ["4", "5", "6", "-"],
@@ -11,8 +11,8 @@ buttons = [
 right_symbols = ["+", "-", "×", "÷", "="]
 top_symbols = ["AC", "+/-", "%"]
 
-row_count = len(buttons) #5
-column_count = len(buttons[0]) #4
+row_count = len(button_values) #5
+column_count = len(button_values[0]) #4
 
 color_light_gray = "#D4D4D2"
 color_black = "#1C1C1C"
@@ -26,7 +26,14 @@ window.resizable(False, False)
 
 frame = tkinter.Frame(window)
 label = tkinter.Label(frame, text="0", font=("Arial", 40), bg=color_black, fg=color_white, width=14, height=2, anchor="e")
-label.pack()
+
+
+label.grid(row=0, column=0)
+
+for row in range(row_count):
+    for column in range(row_count):
+        value = button_values[row][column]
+        button_= tkinter.Button(frame, text=value, font=("Arial", 24), width=column_count-1, height=1, command=lambda v=value: print(v))
 frame.pack()
 window.mainloop()
 
